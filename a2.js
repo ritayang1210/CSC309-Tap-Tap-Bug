a7ffe1cab5bfb439d6371d3c00d44a9382d9f0f1
yang ran <yangran1210@gmail.com>
2015-10-02 23:25:26 -0400

    complete the start view page

--- a2.js
+++ a2.js
@@ -1,4 +1,26 @@
-var canvas = document.getElementById("gameView");
-var ctx = canvas.getContext("2d");
-ctx.fillStyle = "lightgrey";
-ctx.fillRect(0,0,400,650);
+// var canvas = document.getElementById("startView");
+// var ctx = canvas.getContext("2d");
+// ctx.fillStyle = "lightgrey";
+// ctx.fillRect(0,0,400,650);
+
+
+function scoreCounter() {
+    document.getElementById("scoreCounter")
+}
+
+function startGame() {
+    var level1 = document.getElementById("level1");
+    var level2 = document.getElementById("level2");
+    if (typeof(Storage) !== "undefined") {
+        if (level1.checked) {
+            localStorage.setItem("levelSelect", level1.value);
+        } else if (level2.checked) {
+            localStorage.setItem("levelSelect", level2.value);
+        } else {
+            alert("Please select game level!");
+            return;
+        }
+    } else {
+        alert("Sorry! No Web Storage support");
+    }
+}
