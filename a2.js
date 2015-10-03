61a82787f4472bdcfed90f8750c1aab11cd041a5
yang ran <yangran1210@gmail.com>
2015-10-03 02:37:24 -0400

    draw pause and resume button

--- a2.js
+++ a2.js
@@ -4,8 +4,12 @@ ctx.fillStyle = "lightgrey";
 ctx.fillRect(0,0,400,680);
 
 var canvasInfor = document.getElementById("inforBar");
-
-
+var ctxInfor = canvasInfor.getContext("2d");
+ctxInfor.font = "60px Courier New"
+var timerfuc = setInterval(drawTimer, 1000);
+var timer = 61;
+var gameState = drawPause();
+//var gameState = drawResume();
 
 function scoreCounter() {
     document.getElementById("scoreCounter")
@@ -29,3 +33,38 @@ function startGame() {
     }
 }
 
+function drawTimer() {
+    if (timer == 0) {
+        alert("Game Over");
+        clearInterval(timerfuc);
+        return;
+    }
+    timer = timer - 1;
+    ctxInfor.clearRect(0, 0, 100, 200);
+    ctxInfor.fillText(timer + " sec", 10, 100, 50);
+}
+
+function drawPause() {
+    var path=new Path2D();
+    var rectangle = new Path2D();
+    rectangle.rect(140, 50, 5, 60);
+    rectangle.rect(150, 50, 5, 60);
+    ctxInfor.fill(rectangle);
+
+}
+function drawResume() {
+    var path=new Path2D();
+    path.moveTo(160,80);
+    path.lineTo(145,110);
+    path.lineTo(145,50);
+    ctxInfor.fill(path);
+  }
+
+window.addEventListener("mousedown", doMouseDown, false);
+function doMouseDown(event) {
+  x = event.pageX - canvasInfor.offsetLeft;
+  y = event.pageY - canvasInfor.offsetTop;
+  if (x>=30 && x<=60 && y>=30 && y<=60) {
+     alert("BUTTON PRESSED")
+  }
+}
