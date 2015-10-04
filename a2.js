b772b135ae017c32d911ffddc9ff4eae1ad43c66
yang ran <yangran1210@gmail.com>
2015-10-03 22:48:33 -0400

    complete timer

--- a2.js
+++ a2.js
@@ -6,13 +6,21 @@ ctx.fillRect(0,0,400,680);
 var canvasInfor = document.getElementById("inforBar");
 var ctxInfor = canvasInfor.getContext("2d");
 ctxInfor.font = "60px Courier New"
+
+var timer = 60;
 var timerfuc = setInterval(drawTimer, 1000);
-var timer = 61;
-var gameState = drawPause();
-//var gameState = drawResume();
+
+var pause = false;
+changeState();
+
+var highestScore = 0;
+var score = 0;
 
 function scoreCounter() {
-    document.getElementById("scoreCounter")
+    if (score >= highestScore) {
+        localStorage.setItem("highestScore", score);
+    }
+    document.getElementById("scoreCounter").textContent = localStorage.getItem("highestScore");
 }
 
 function startGame() {
@@ -38,33 +46,47 @@ function drawTimer() {
         alert("Game Over");
         clearInterval(timerfuc);
         return;
+    } else if (pause==true) {
+        timer = timer - 1;
+        ctxInfor.clearRect(0, 0, 100, 200);
+        ctxInfor.fillText(timer + " sec", 10, 100, 50);
     }
-    timer = timer - 1;
-    ctxInfor.clearRect(0, 0, 100, 200);
-    ctxInfor.fillText(timer + " sec", 10, 100, 50);
+}
+
+function changeState() {
+    if (pause == false) {
+        drawPause();
+        pause = true;
+    } else if (pause == true) {
+        drawResume();
+        pause = false;
+    }
 }
 
 function drawPause() {
+    ctxInfor.clearRect(100, 0, 100, 200);
     var path=new Path2D();
     var rectangle = new Path2D();
     rectangle.rect(140, 50, 5, 60);
     rectangle.rect(150, 50, 5, 60);
     ctxInfor.fill(rectangle);
-
 }
+
 function drawResume() {
+    ctxInfor.clearRect(100, 0, 100, 200);
+
     var path=new Path2D();
     path.moveTo(160,80);
     path.lineTo(145,110);
     path.lineTo(145,50);
     ctxInfor.fill(path);
-  }
+}
 
 window.addEventListener("mousedown", doMouseDown, false);
 function doMouseDown(event) {
   x = event.pageX - canvasInfor.offsetLeft;
   y = event.pageY - canvasInfor.offsetTop;
-  if (x>=30 && x<=60 && y>=30 && y<=60) {
-     alert("BUTTON PRESSED")
+  if (x>=190 && x<=220 && y>=27 && y<=60) {
+     changeState();
   }
 }
