dd3163a62b93e1da75aa3adb89261f7a405303aa
yang ran <yangran1210@gmail.com>
2015-10-06 02:40:00 -0400

    bugs move

--- a2.js
+++ a2.js
@@ -15,9 +15,14 @@ var color = 'black';
 var xCoord = canvasGame.width / 2;
 var yCoord = canvasGame.height / 2;
 var bugRot = 0;
-var bugfuc = setInterval(moveBugs, 1000);
+//var bugfuc = setInterval(moveBugs, 1000);
 
+var foodList = [];
 drawFoods();
+var newBug = new Bug();
+newBug.draw();
+setInterval(function() { newBug.move() }, 100);
+window.addEventListener("mousedown", doMouseDown, false);
 
 function scoreCounter() {
     var highestScore = 2;
@@ -86,89 +91,6 @@ function drawResume() {
     ctxInfor.fill(path);
 }
 
-function drawBugs(xCoord, yCoord, color, rot) {
-    ctxGame.save();
-
-    var path = new Path2D();
-    ctxGame.fillStyle = color;
-    ctxGame.strokeStyle = color;
-
-    ctxGame.translate(xCoord, yCoord);
-    ctxGame.rotate(rot);
-
-    path.arc(0, -17, 4, 0, 2 * Math.PI, false);
-    path.ellipse(0, 0, 13, 3, 90 * Math.PI/180, 0, 2 * Math.PI);
-    ctxGame.fill(path);
-
-    ctxGame.beginPath();
-
-    ctxGame.moveTo(0, -17);
-    ctxGame.lineTo(-5, -27);
-
-    ctxGame.moveTo(0, -17);
-    ctxGame.lineTo(5, -27);
-
-    ctxGame.moveTo(0, -3);
-    ctxGame.lineTo(5, -10);
-    ctxGame.lineTo(6, -15);
-
-    ctxGame.moveTo(0, -3);
-    ctxGame.lineTo(-5, -10);
-    ctxGame.lineTo(-6, -15);
-
-    ctxGame.moveTo(0, -4);
-    ctxGame.lineTo(5, -4);
-    ctxGame.lineTo(5, -1);
-
-    ctxGame.moveTo(0, -4);
-    ctxGame.lineTo(-5, -4);
-    ctxGame.lineTo(-5, -1);
-
-    ctxGame.moveTo(0, -3);
-    ctxGame.lineTo(5, 3);
-    ctxGame.lineTo(6, 8);
-
-    ctxGame.moveTo(0, -3);
-    ctxGame.lineTo(-5, 3);
-    ctxGame.lineTo(-6, 8);
-
-    ctxGame.stroke();
-
-    ctxGame.restore();
-}
-
-function moveBugs(angle) {
-    ctxGame.save();
-    ctxGame.translate(xCoord, yCoord);
-    ctxGame.rotate(bugRot);
-    ctxGame.clearRect(-7, -28, 14, 42);
-    ctxGame.restore();
-    bugRot += 0.1;
-    drawBugs(xCoord, yCoord, "black", bugRot);
-}
-
-function drawFoods() {
-    var foodList = [];
-    for (i = 0; i < 5; i++) {
-        var newFood = new food();
-        while (newFood.overlapWith(foodList)) {
-            newFood = new food();
-        }
-        newFood.draw();
-        foodList.push(newFood);
-    }
-}
-
-
-window.addEventListener("mousedown", doMouseDown, false);
-function doMouseDown(event) {
-  x = event.pageX - canvasInfor.offsetLeft;
-  y = event.pageY - canvasInfor.offsetTop;
-  if (x>=190 && x<=220 && y>=27 && y<=60) {
-     changeState();
-  }
-}
-
 function food() {
     this.xCoord = Math.floor(Math.random() * (381) + 10);
     this.yCoord = Math.floor(Math.random() * (461) + 130);
@@ -191,3 +113,136 @@ function food() {
         return false;
     }
 }
+
+function drawFoods() {
+    for (i = 0; i < 5; i++) {
+        var newFood = new food();
+        while (newFood.overlapWith(foodList)) {
+            newFood = new food();
+        }
+        newFood.draw();
+        foodList.push(newFood);
+    }
+}
+
+function removeFoods() {
+    //TODO
+}
+
+function Bug() {
+    "use strict";
+    var alive = true;
+    this.xCoord = Math.floor(Math.random() * 400);
+    this.yCoord = 0;
+
+    this.findNearestFood = function() {
+        var minDist = Number.MAX_VALUE;
+        var result = null;
+        for (i = 0; i < foodList.length; i++) {
+            var food = foodList[i];
+            var dist = Math.sqrt(Math.pow(this.xCoord - food.xCoord, 2) + Math.pow(this.yCoord - food.yCoord, 2));
+            if (food.eaton == false && dist < minDist) {
+                result = food;
+                minDist = dist;
+            }
+        }
+        return result;
+    }
+
+    this.chooseColor = function() {
+        var colorArray = ["black", "red", "orange"];
+        var num = Math.random();
+        if (num < 0.3) {
+            return colorArray[0];
+        } else if (0.3 <= num && num < 0.6) {
+            return colorArray[1];
+        } else {
+            return colorArray[2];
+        }
+    }
+
+    this.getDirection = function() {
+        return Math.atan2(this.targetFood.yCoord - this.yCoord, this.targetFood.xCoord - this.xCoord) + Math.PI / 2;
+    }
+
+    this.color = this.chooseColor();
+    this.targetFood = this.findNearestFood();
+    this.direction = this.getDirection();
+
+    this.draw = function() {
+        ctxGame.save();
+        var path = new Path2D();
+        
+
+        ctxGame.fillStyle = this.color;
+        ctxGame.strokeStyle = this.color;
+
+        ctxGame.translate(this.xCoord, this.yCoord);
+        ctxGame.rotate(this.direction);
+
+        path.arc(0, -17, 4, 0, 2 * Math.PI, false);
+        path.ellipse(0, 0, 13, 3, 90 * Math.PI/180, 0, 2 * Math.PI);
+        ctxGame.fill(path);
+
+        ctxGame.beginPath();
+
+        ctxGame.moveTo(0, -17);
+        ctxGame.lineTo(-5, -27);
+
+        ctxGame.moveTo(0, -17);
+        ctxGame.lineTo(5, -27);
+
+        ctxGame.moveTo(0, -3);
+        ctxGame.lineTo(5, -10);
+        ctxGame.lineTo(6, -15);
+
+        ctxGame.moveTo(0, -3);
+        ctxGame.lineTo(-5, -10);
+        ctxGame.lineTo(-6, -15);
+
+        ctxGame.moveTo(0, -4);
+        ctxGame.lineTo(5, -4);
+        ctxGame.lineTo(5, -1);
+
+        ctxGame.moveTo(0, -4);
+        ctxGame.lineTo(-5, -4);
+        ctxGame.lineTo(-5, -1);
+
+        ctxGame.moveTo(0, -3);
+        ctxGame.lineTo(5, 3);
+        ctxGame.lineTo(6, 8);
+
+        ctxGame.moveTo(0, -3);
+        ctxGame.lineTo(-5, 3);
+        ctxGame.lineTo(-6, 8);
+
+        ctxGame.stroke();
+        ctxGame.restore();
+    }
+
+    this.move = function() {
+        this.targetFood = this.findNearestFood();
+        var rightDirection = this.getDirection();
+        if (this.direction - rightDirection > 0.01) {
+            this.direction += ((rightDirection - this.direction) / 2);
+        } else {
+            this.xCoord += 10 * Math.sin(this.direction);
+            this.yCoord -= 10 * Math.cos(this.direction);
+        }
+        ctxGame.save();
+        ctxGame.translate(this.xCoord, this.yCoord);
+        ctxGame.rotate(this.direction);
+        ctxGame.clearRect(-7, -28, 14, 42);
+        ctxGame.restore();
+        this.draw();
+    }
+}
+
+function doMouseDown(event) {
+  x = event.pageX - canvasInfor.offsetLeft;
+  y = event.pageY - canvasInfor.offsetTop;
+  if (x >= 190 && x <= 220 && y >= 27 && y <=60) {
+     changeState();
+  }
+}
+
